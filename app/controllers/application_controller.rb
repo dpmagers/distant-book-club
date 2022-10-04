a3401a95396d6cd0f700c5013251a6ce42d13463
Daniel Magers <daniel.magers@gmail.com>
2022-10-03 19:59:49 -0500

    backend set up w/o CRUD. Routes not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-
-    # def hello_world
-    #     session[:count] = (session[:count] || 0) + 1
-    #     render json: { count: session[:count] }
-    #   end
-
     before_action :authorize
 
     private
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+
+    
+    # def index
+    #     render json: Book.all, status: :ok
+    # end 
+
+    # def show
+    #     book = Book.find(params[:id])
+    #     render json: book, status: :ok
+    # end 
 end
